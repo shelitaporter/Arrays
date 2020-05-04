69f23683f94427e96ec64143ddfa8f00bad344c0
shelitaporter <lita200296@yahoo.com>
2020-05-03 20:18:45 -0500

    commit on 'master'

---
+++ index.js
@@ -0,0 +1,26 @@
+const arr = [
+    {name: 'chevy', count: 2},
+    {name: 'ford', count: 5},
+    {name: 'acura', count: 3},
+    {name: 'honda', count: 16},
+].filter(filtered);
+
+console.log("Filter results:", arr);
+
+
+var filtered = car => {
+    return car.name === 'ford';
+};
+
+
+
+
+
+
+
+const arr = ['Bill', 'Joe', 'Emily', 'Andrea']
+const newStudents = ['Andrew', 'Tasha', 'Carol', 'George'];
+
+const fullArray = [...arr, ...newStudents];
+
+console.log(fullArray);
